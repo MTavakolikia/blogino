4cc3784f6f2e95a27249d2bd2c836ce50da876f2
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2025-02-15 21:15:09 +0330

    init sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,8 @@
-"use client";
 
-import { useEffect, useState } from "react";
-import { User } from "@/types/user";
-
-export default function Dashboard() {
-    const [user, setUser] = useState<User | null>(null);
-
-    useEffect(() => {
-        const stringifiedUser = sessionStorage.getItem("user");
-        if (stringifiedUser) {
-            setUser(JSON.parse(stringifiedUser));
-        }
-    }, []);
-
-    if (!user) {
-        return <p>لطفاً وارد شوید.</p>;
-    }
-
+function Home() {
     return (
-        <div>
-            <h1>داشبورد</h1>
-            <p>سلام، {user.firstName} {user.lastName}! نقش شما: {user.role}</p>
-        </div>
-    );
+        <div>page</div>
+    )
 }
+
+export default Home
